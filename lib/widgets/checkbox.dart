b08a5784392c8980772a5876070508608bbdb20f
kannan19302 <kannan19302@gmail.com>
2026-08-09 22:20:41 +0530

    fix(widget): update checkbox widget component

--- lib/widgets/checkbox.dart
+++ lib/widgets/checkbox.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Checkbox extends StatelessWidget {
-  const Checkbox({Key? key}) : super(key: key);
+  const Checkbox({super.key});
 
   @override
   Widget build(BuildContext context) {
